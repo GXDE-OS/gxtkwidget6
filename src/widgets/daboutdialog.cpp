cabf3d0998d19b4445fd2853e83e790931889d5c
gfdgd_xi <3025613752@qq.com>
2024-10-05 22:48:49 +0800

    修改域名

--- src/widgets/daboutdialog.cpp
+++ src/widgets/daboutdialog.cpp
@@ -176,14 +176,14 @@ void DAboutDialogPrivate::initWebsiteInfo()
 
     //websiteName = isProfessional ? "www.deepin.com" : "www.deepin.org";
     //websiteLink = QString("https://www.deepin.org/original/%1/").arg(qApp->applicationName());
-    websiteName = "www.gxde.top";
-    websiteLink = "https://www.gxde.top";
+    websiteName = "www.gxde.org";
+    websiteLink = "https://www.gxde.org";
 
 #else
     //websiteName = "www.deepin.org";
     //websiteLink = "https://www.deepin.org";
-    websiteName = "www.gxde.top";
-    websiteLink = "https://www.gxde.top";
+    websiteName = "www.gxde.org";
+    websiteLink = "https://www.gxde.org";
 #endif
 }
 
